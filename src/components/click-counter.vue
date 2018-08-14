5eab7179b1f3713ff57663029a652a8610005e8f
cest <cestrmail@163.com>
2018-08-14 17:20:48 +0800

    # 添加计数器

---
+++ src/components/click-counter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="click-couter">
+        <div class="counter-num">次数:{{num}}</div>
+        <button class="counter-btn" @click="handleClick">点我呀</button>
+        <button class="couter-reset-btn" @click="handleResetClick">清零</button> 
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      num: 0
+    }
+  },
+
+  methods: {
+    handleClick () {
+      this.num += 1
+    },
+    handleResetClick () {
+      this.num = 0
+    }
+  }
+}
+</script>
+
+<style>
+.click-couter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
+  background-color: #fff;
+  padding: 10px;
+}
+.counter-num,
+.counter-btn,
+.couter-reset-btn {
+  flex: 1;
+  margin: 3px;
+}
+</style>
+
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,111 +1,35 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <div>getProductsAPI</div>
-    <div :text="getProductsAPI"></div>
+  <div class="container" @click="clickHandle">
+    <div class="message">{{msg}}</div>
+    <click-counter/>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
-
+// 导入 click-counter 组件
+import ClickCounter from '@/components/click-counter'
 export default {
+  components: {
+    // 声明在当前组件下使用 counter-click 组件
+    ClickCounter
+  },
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {},
-      api: '',
-      getProductsAPI: ''
+      msg: 'Hello'
     }
   },
-
-  components: {
-    card
-  },
-
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: res => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    clickHandle () {
+      this.msg = 'Clicked!!!!!!'
     }
-  },
-
-  created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
-    const baseURL = process.env.API_BASE_URL
-    this.api = baseURL
-    this.getProductsAPI = `${baseURL}/products`
   }
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
-}
-
-.counter {
-  display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+.message {
+  color: red;
+  padding: 10px;
+  text-align: center;
 }
 </style>
